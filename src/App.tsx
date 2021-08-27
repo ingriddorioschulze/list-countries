9479437b9cfdcf5663a8804a38fcaabdbbf6c379
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2021-08-27 10:42:54 +0200

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from './Header'
 
-function App() {
-  return <div className="App">list countries</div>
+const App: React.FC = (): JSX.Element => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
 }
 
 export default App
---
+++ src/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Header: React.FC = (): JSX.Element => {
+  return <div>list countries</div>
+}
+
+export default Header
